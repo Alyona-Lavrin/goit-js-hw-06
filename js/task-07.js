94536d80a1497046db362f3713d53f9e691f0100
Alona <polejcukalona@gmail.com>
2023-07-25 22:17:57 +0300

    hjkfesh

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,7 @@
-const input = document.querySelector("#font-size-control").value="16";
+const input = document.querySelector("#font-size-control");
+input.value = 22;
 const text = document.querySelector("#text");
+text.style.fontSize = '22px';
 input.addEventListener("input", fn1);
 function fn1(e) {
   text.style.fontSize = `${e.target.value}px`;
